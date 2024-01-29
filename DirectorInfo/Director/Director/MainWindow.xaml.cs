f7015cd620f54505d765b79cdb49a6b96cb743b4
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2024-01-30 03:07:23 +0700

    client-server do Q1_SendToServer director infor

--- DirectorInfo/Director/Director/MainWindow.xaml.cs
+++ DirectorInfo/Director/Director/MainWindow.xaml.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,7 +26,6 @@ namespace DirectorInfo
         private readonly PE_PRN_Fall2023B1Context _context;
         public MainWindow()
         {
-            InitializeComponent();
         }
         public MainWindow(PE_PRN_Fall2023B1Context context)
         {
@@ -35,9 +36,19 @@ namespace DirectorInfo
         public void UpdateGridView()
         {
             List<DirectorDTO> directors = _context.Directors.ToList()
-                .Select(item => MapToDirectorDTO(item)).ToList();
+                .Select(item => new DirectorDTO()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Dob = item.Dob,
+                    Description = item.Description,
+                    Male = item.Male,
+                    GenderString = item.Male == true ? "Male" : "Female",
+                    Nationality = item.Nationality
+                }).ToList();
 
             lsDirector.ItemsSource = directors;
+            tempDirectorList = directors;
         }
         public void LoadData()
         {
@@ -49,11 +60,11 @@ namespace DirectorInfo
             {
                 return new Director
                 {
-                    Id = string.IsNullOrEmpty(txtId.Text)? 0: int.Parse(txtId.Text),
+                    Id = string.IsNullOrEmpty(txtId.Text) ? 0 : int.Parse(txtId.Text),
                     Name = txtName.Text,
                     Description = txtDescription.Text,
                     Dob = dpDob.SelectedDate,
-                    Male = cbMale.IsChecked == true? true:false,
+                    Male = cbMale.IsChecked == true ? true : false,
                     Nationality = txtNationality.Text
                 };
 
@@ -69,10 +80,42 @@ namespace DirectorInfo
         {
 
         }
+        private List<DirectorDTO> tempDirectorList = new List<DirectorDTO>();
 
         private void btnSendToServer_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var directorInfo = tempDirectorList;
+                string jsonData = JsonSerializer.Serialize(directorInfo, new JsonSerializerOptions { WriteIndented = true });
+                string host = "127.0.0.1";
+                int port = 5000;
+                int bytes;
+                try
+                {
+                    TcpClient tcpClient = new TcpClient(host, port);
+                    NetworkStream stream = null;
+                    while (true)
+                    {
+                        Byte[] data = Encoding.ASCII.GetBytes(jsonData);
+                        MessageBox.Show(data + "");
+                        stream = tcpClient.GetStream();
+                        stream.Write(data, 0, data.Length);
+                        break;
 
+                    }
+                    tcpClient.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("{0}", ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void btnAddToList_Click(object sender, RoutedEventArgs e)
@@ -86,7 +129,7 @@ namespace DirectorInfo
                     _context.Directors.Add(director);
                     _context.SaveChanges();
                     UpdateGridView();
-                    MessageBox.Show("Insert Director success " + director.Id +" "+ director.Name, "Add Director");
+                    MessageBox.Show("Insert Director success " + director.Id + " " + director.Name, "Add Director");
                 }
             }
             catch (Exception ex)
@@ -98,9 +141,9 @@ namespace DirectorInfo
         private void listView_Click(object sender, MouseButtonEventArgs e)
         {
             var item = (sender as ListView).SelectedItem;
-            if(item != null )
+            if (item != null)
             {
-                cbMale.IsChecked = ((DirectorDTO)item).Male == true? true : false;
+                cbMale.IsChecked = ((DirectorDTO)item).Male == true ? true : false;
             }
         }
 
@@ -109,27 +152,22 @@ namespace DirectorInfo
             var searchText = txtSearch.Text;
             List<DirectorDTO> directors = new List<DirectorDTO>();
             var listDirector = _context.Directors
-                .Where(i => (i.Name.Contains(searchText) || (i.Description != null && i.Description.Contains(searchText)))
-                || (searchText != null)//nếu k có đk 
+                .Where(i => i.Name.Contains(searchText)
                 )
-                .Select(item => MapToDirectorDTO(item))
+                .Select(item => new DirectorDTO()
+                {
+                    Id = item.Id,
+                    Name = item.Name,
+                    Dob = item.Dob,
+                    Description = item.Description,
+                    Male = item.Male,
+                    GenderString = item.Male == true ? "Male" : "Female",
+                    Nationality = item.Nationality
+                })
                 .ToList();
-  
-            lsDirector.ItemsSource = directors;
 
-        }
-        private DirectorDTO MapToDirectorDTO(Director item)
-        {
-            return new DirectorDTO()
-            {
-                Id = item.Id,
-                Name = item.Name,
-                Dob = item.Dob,
-                Description = item.Description,
-                Male = item.Male,
-                GenderString = item.Male == true ? "Male" : "Female",
-                Nationality = item.Nationality
-            };
+            lsDirector.ItemsSource = listDirector;
+
         }
         private void btnLoadData_Click(object sender, RoutedEventArgs e)
         {
---
+++ TCPService_SE1711/TCPService_SE1711/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCPService_SE1711
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListen_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new Thread(Listen);
+            thread.Start();
+        }
+        public void Listen()
+        {
+            string host = "127.0.0.1";
+            int port = 5000;
+            int count = 0; //đếm số lượng client kết nối với server 
+            TcpListener server = null;
+            try
+            {
+                IPAddress localAddress = IPAddress.Parse(host);
+                server = new TcpListener(localAddress, port);
+                server.Start();
+                Dispatcher.Invoke(() =>
+                {
+                    txtContent.Text = $"waiting for client connection ... ";
+
+                });
+                while(true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Dispatcher.Invoke(() =>
+                    {
+                        txtContent.Text = $"Number of client connected ..." + ++count;
+
+                    });
+                    //thread lắng nghe client gửi nhận dữ liệu 
+                    Thread newThread = new Thread(new ParameterizedThreadStart(ProcessClient));
+                    newThread.Start(client);
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void ProcessClient(object p)
+        {
+            string data; int count;
+            try
+            {
+                TcpClient client = p as TcpClient;
+                Byte[] bytes = new Byte[1024];
+                NetworkStream stream = client.GetStream();
+                while ((count = stream.Read(bytes, 0, bytes.Length)) > 0)
+                {
+                    data = Encoding.ASCII.GetString(bytes, 0, count);
+                    Dispatcher.Invoke(() =>
+                    {
+                        txtContent.Text = txtContent.Text + "\r\n" + data;
+                    });
+                }
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+ 
+
+
+    }
+}
