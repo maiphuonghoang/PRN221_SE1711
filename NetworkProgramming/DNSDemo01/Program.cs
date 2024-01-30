3845eba1a03906b03880ada048230d72d3d6552c
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2024-01-30 09:23:49 +0700

    run the demo TCPServer & NetworkProgramming in slide

---
+++ NetworkProgramming/DNSDemo01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(new string('*', 30));
+        var domainEntry = Dns.GetHostEntry("www.contoso.com");
+        Console.WriteLine(domainEntry.HostName);
+        foreach (var ip in domainEntry.AddressList)
+        {
+            Console.WriteLine(ip);
+        }
+        Console.WriteLine(new string('*', 30));
+        var domainEntryByAddress = Dns.GetHostEntry("127.0.0.1");
+        Console.WriteLine(domainEntryByAddress.HostName);
+        foreach (var ip in domainEntryByAddress.AddressList)
+        {
+            Console.WriteLine(ip);
+        }
+        //my test 
+        Console.WriteLine(new string('*', 30));
+        var domainEntry2 = Dns.GetHostEntry("cmshn.fpt.edu.vn");
+        Console.WriteLine(domainEntry2.HostName);
+        foreach (var ip in domainEntry2.AddressList)
+        {
+            Console.WriteLine(ip);
+        }
+        Console.WriteLine(new string('*', 30));
+        var domainEntry3 = Dns.GetHostEntry("www.google.com");
+        Console.WriteLine(domainEntry3.HostName);
+        foreach (var ip in domainEntry3.AddressList)
+        {
+            Console.WriteLine(ip);
+        }
+    }
+}
---
+++ NetworkProgramming/URIsDemo01/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Console;
+class Program
+{
+    static void Main(string[] args)
+
+    {
+
+        Uri info = new Uri("http://www.domain.com:80/info?id=123#fragment");
+        Uri page = new Uri("http://www.domain.com/info/page.html");
+        //Uri info = new Uri("https://cmshn.fpt.edu.vn/mod/folder/view.php?id=157360");
+        //Uri page = new Uri("https://cmshn.fpt.edu.vn/mod/folder/view.php");
+        WriteLine($"Host: {info.Host}");
+
+        WriteLine($"Port: {info.Port}");
+
+        WriteLine($"PathAndQuery: {info.PathAndQuery}");
+        WriteLine($"Query: {info.Query}");
+
+        WriteLine($"Fragment: {info.Fragment}");
+        WriteLine($"Default HTTP port: {page.Port}");
+        WriteLine($"IsBaseOf: {info.IsBaseOf(page)}");
+
+        Uri relative = info.MakeRelativeUri(page);
+        WriteLine($"IsAbsoluteUri: {relative.IsAbsoluteUri}");
+        WriteLine($"RelativeUri: {relative.ToString()}");
+        ReadLine();
+
+    }
+}
