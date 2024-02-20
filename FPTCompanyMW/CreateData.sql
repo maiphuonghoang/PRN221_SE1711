f7dd479a51d60f73c008f93c128ff9a3700d0eaf
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2024-02-20 09:54:44 +0700

    database group, job, salary for employee

---
+++ FPTCompanyMW/CreateData.sql
@@ -0,0 +1,118 @@
+﻿﻿-- DROP DATABASE PRN221_FPTCompanyMW;
+-- CREATE DATABASE PRN221_FPTCompanyMW;
+USE PRN221_FPTCompanyMW; 
+
+CREATE TABLE Account (
+    username VARCHAR(150) NOT NULL PRIMARY KEY,
+    [password] VARCHAR(150) NOT NULL,
+);
+CREATE TABLE [Role](
+	roleId int NOT NULL,
+	roleName varchar(150),
+	CONSTRAINT PK_Role PRIMARY KEY (roleId)
+)
+CREATE TABLE AccountRole(
+	username varchar(150) NOT NULL,
+	roleId int NOT NULL,
+	CONSTRAINT PK_AccountRole PRIMARY KEY (username, roleId)
+)
+
+ALTER TABLE AccountRole ADD CONSTRAINT FK_AccountRole_Account FOREIGN KEY(username)
+REFERENCES Account (username)
+ALTER TABLE AccountRole ADD CONSTRAINT FK_AccountRole_Role FOREIGN KEY(roleId)
+REFERENCES [Role] (roleId)
+
+CREATE TABLE Employee
+(
+	employeeId varchar(50) NOT NULL,
+	employeeName nvarchar(100),
+	gender bit,
+	employeeImage varchar(max),
+	CONSTRAINT PK_Employee PRIMARY KEY (employeeId),
+
+	accountId varchar(150) NOT NULL UNIQUE,
+    CONSTRAINT FK_Employee_Account FOREIGN KEY (accountId) REFERENCES Account(username)
+ )
+
+ CREATE TABLE [Group]
+(
+	groupCode varchar(10) NOT NULL,
+	groupName varchar(20) NULL,
+	buId varchar(50) NULL,
+	CONSTRAINT PK_Group PRIMARY KEY (groupCode)
+)
+ALTER TABLE [Group] ADD CONSTRAINT FK_Group_Employee FOREIGN KEY(buId)
+REFERENCES Employee (employeeId)
+
+CREATE TABLE Participate
+(
+	groupCode varchar(10) NOT NULL,
+	employeeId varchar(50) NOT NULL,
+	CONSTRAINT PK_Participate PRIMARY KEY (groupCode, employeeId)
+)
+
+CREATE TABLE Job (
+    jobCode VARCHAR(10) PRIMARY KEY,
+    jobName VARCHAR(255) NOT NULL
+);
+CREATE TABLE Package (
+    packageCode VARCHAR(10) PRIMARY KEY,
+    packageSalary DECIMAL(18, 0) NOT NULL
+);
+
+CREATE TABLE JobRank (
+    jobRankId INT PRIMARY KEY,
+    jobCode VARCHAR(10) REFERENCES Job(jobCode),
+    jobRank VARCHAR(10) NOT NULL,
+    packageCode VARCHAR(10) REFERENCES Package(packageCode)
+);
+
+CREATE TABLE Salary 
+(
+    employeeId VARCHAR(50) REFERENCES Employee(employeeId),
+    jobRankId INT REFERENCES JobRank(jobRankId),
+	PRIMARY KEY (employeeId, jobRankId)
+);
+
+SELECT * FROM Employee;
+SELECT * FROM AccountRole;
+SELECT * FROM Account;
+SELECT * FROM [Role]
+
+SELECT * FROM [Group]
+SELECT * FROM Participate
+SELECT * FROM Job
+SELECT * FROM Package
+SELECT * FROM JobRank
+SELECT * FROM Salary
+
+DELETE FROM Participate
+DELETE FROM Job
+DELETE FROM Package
+DELETE FROM JobRank
+DELETE FROM Salary
+/*
+DROP TABLE [Group]
+DROP TABLE Participate
+DROP TABLE Job
+DROP TABLE Package
+DROP TABLE JobRank
+DROP TABLE Salary
+*/
+
+SELECT employeeId FROM Participate
+GROUP BY employeeId
+HAVING COUNT(DISTINCT groupCode) > 1;
+
+
+SELECT * FROM [Group] g JOIN Participate p ON g.groupCode = p.groupCode WHERE g.groupCode = 'IVS';
+
+SELECT e.employeeId, e.employeeName, g.groupCode, j.jobCode, j.jobName, jr.jobRank, jr.packageCode, pk.packageSalary 
+FROM [Group] g JOIN Participate p ON g.groupCode = p.groupCode 
+JOIN Employee e ON e.employeeId = p.employeeId 
+JOIN Salary s ON s.employeeId = e.employeeId
+JOIN JobRank jr ON jr.jobRankId = s.jobRankId
+JOIN Job j ON j.jobCode = jr.jobCode
+JOIN Package pk ON pk.packageCode = jr.packageCode
+WHERE g.groupCode IN ('IVS')
+-- WHERE j.jobCode IN ('SPMA', 'BEDEVE')
---
+++ FPTCompanyMW/InsertData.sql
@@ -0,0 +1,451 @@
+INSERT INTO [Group] (groupCode, buId) VALUES ('GHC', 'thangnv75');
+INSERT INTO [Group] (groupCode, buId) VALUES ('EBS', 'anhnh88');
+INSERT INTO [Group] (groupCode, buId) VALUES ('GST', 'sonnt5');
+INSERT INTO [Group] (groupCode, buId) VALUES ('IVS', 'tientd17');
+INSERT INTO [Group] (groupCode, buId) VALUES ('FCJ', 'trungnt');
+
+
+--15 
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE150057', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE151095', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE153206', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE160694', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE161357', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE161795', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE163146', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE164035', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170051', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170245', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170422', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170428', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170444', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170533', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170842', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170863', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170907', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE170996', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171071', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171073', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171162', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171442', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171482', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171578', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171687', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171851', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE171865', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE176182', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE176697', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('HE176751', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('SE03520', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('SE04495', 'GHC');
+INSERT INTO Participate (employeeId, groupCode) VALUES ('thangnv75', 'GHC');
+
+
+
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'DE170703')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE153678')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE161700')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE161706')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE161755')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE163415')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE163635')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE164044')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE170072')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE170180')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE170201')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE170203')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE170420')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE170595')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE171098')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE171319')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE171320')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE171416')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE171702')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE171937')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE172032')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE173037')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE173046')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE176640')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE181829')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HE181879')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HS170024')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HS170726')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HS173175')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('EBS', 'HS176033')
+INSERT INTO Participate (employeeId, groupCode) VALUES ('anhnh88', 'EBS');
+
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE161232')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE161643')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE163665')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE170123')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE170268')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE170302')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE170314')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE170317')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE170617')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE171209')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE171310')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE171883')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE172207')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE172366')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE172533')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE172693')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE172802')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE173328')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE173373')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE173476')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE173489')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176275')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176282')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176285')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176346')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176360')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176603')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176686')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('FCJ', 'HE176834')
+INSERT INTO Participate (employeeId, groupCode) VALUES ('trungnt', 'FCJ');
+
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE161285')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE161816')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE163067')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170011')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170167')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170273')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170301')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170415')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170607')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170622')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170631')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170724')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170960')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE170999')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE171312')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE171411')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE171540')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE171628')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE172037')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE172748')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE172838')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE172848')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE173248')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE173588')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE176077')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE176087')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE176169')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE176420')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('GST', 'HE176882')
+INSERT INTO Participate (employeeId, groupCode) VALUES ('sonnt5', 'GST');
+
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE150732')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE150998')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE161224')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE161597')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE163629')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE170089')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE170685')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE170793')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171358')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171394')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171421')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171552')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171908')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171915')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171916')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE171990')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172180')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172280')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172313')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172435')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172527')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172538')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172702')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE172737')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE173401')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE173457')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE176211')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE176279')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE176633')
+INSERT INTO Participate (groupCode, employeeId) VALUES ('IVS', 'HE176747')
+INSERT INTO Participate (employeeId, groupCode) VALUES ('tientd17', 'IVS');
+
+INSERT INTO Job (jobCode, jobName)
+VALUES
+('SPMA', 'Project Manager'),
+('BRSE', 'Bridge Software Engineering'),
+('SDSM', 'Scrum Master'),
+('SPOW', 'Product Owner'),
+('BANE', 'Business Analyst'),
+('DESI', 'Designer'),
+('SARE', 'Solution Architect'),
+('BEDEVE', 'Back-end Developer'),
+('MBDEVE', 'Mobile Developer'),
+('FEDEVE', 'Front-end Developer'),
+('ODEV', 'OS and Middleware Developer'),
+('TEST', 'Tester'),
+('AWSC', 'AWS Consultant'),
+('DSEN', 'Data Engineer'),
+('DSAN', 'Data Analyst'),
+('DEVO', 'DevOps Engineer');
+
+INSERT INTO Package (packageCode, packageSalary)
+VALUES
+('P2', 8000000),
+('P3', 10000000),
+('P4', 15000000),
+('P5', 18000000),
+('P6', 20000000),
+('P7', 25000000),
+('P8', 28000000),
+('P9', 30000000);
+
+-- Insert data into JobRank table
+INSERT INTO JobRank (jobRankId, jobCode, jobRank, packageCode)
+VALUES
+(1, 'SPMA', 'SPMA01', 'P4'),
+(2, 'SPMA', 'SPMA02', 'P5'),
+(3, 'SPMA', 'SPMA03', 'P6'),
+(4, 'SPMA', 'SPMA04', 'P7'),
+(5, 'SPMA', 'SPMA05', 'P8'),
+(6, 'SPMA', 'SPMA06', 'P9'),
+(7, 'BRSE', 'BRSE01', 'P4'),
+(8, 'BRSE', 'BRSE02', 'P5'),
+(9, 'BRSE', 'BRSE03', 'P6'),
+(10, 'BRSE', 'BRSE04', 'P7'),
+(11, 'BRSE', 'BRSE05', 'P8'),
+(12, 'SDSM', 'SDSM01', 'P3'),
+(13, 'SDSM', 'SDSM02', 'P4'),
+(14, 'SDSM', 'SDSM03', 'P5'),
+(15, 'SDSM', 'SDSM04', 'P6'),
+(16, 'SPOW', 'SPOW01', 'P4'),
+(17, 'SPOW', 'SPOW02', 'P5'),
+(18, 'SPOW', 'SPOW03', 'P6'),
+(19, 'SPOW', 'SPOW04', 'P7'),
+(20, 'SPOW', 'SPOW05', 'P8'),
+(21, 'SPOW', 'SPOW06', 'P9'),
+(22, 'BANE', 'BANE01', 'P3'),
+(23, 'BANE', 'BANE02', 'P4'),
+(24, 'BANE', 'BANE03', 'P5'),
+(25, 'BANE', 'BANE04', 'P6'),
+(26, 'BANE', 'BANE05', 'P7'),
+(27, 'BANE', 'BANE06', 'P8'),
+(28, 'DESI', 'DESI01', 'P4'),
+(29, 'DESI', 'DESI02', 'P5'),
+(30, 'DESI', 'DESI03', 'P6'),
+(31, 'DESI', 'DESI04', 'P7'),
+(32, 'DESI', 'DESI05', 'P8'),
+(33, 'DESI', 'DESI06', 'P9'),
+(34, 'SARE', 'SARE01', 'P5'),
+(35, 'SARE', 'SARE02', 'P6'),
+(36, 'SARE', 'SARE03', 'P7'),
+(37, 'SARE', 'SARE04', 'P8'),
+(38, 'SARE', 'SARE05', 'P9'),
+(39, 'BEDEVE', 'DEVE01', 'P2'),
+(40, 'BEDEVE', 'DEVE02', 'P3'),
+(41, 'BEDEVE', 'DEVE03', 'P4'),
+(42, 'BEDEVE', 'DEVE04', 'P5'),
+(43, 'BEDEVE', 'DEVE05', 'P6'),
+(44, 'BEDEVE', 'DEVE06', 'P7'),
+(45, 'MBDEVE', 'DEVE01', 'P2'),
+(46, 'MBDEVE', 'DEVE02', 'P3'),
+(47, 'MBDEVE', 'DEVE03', 'P4'),
+(48, 'MBDEVE', 'DEVE04', 'P5'),
+(49, 'MBDEVE', 'DEVE05', 'P6'),
+(50, 'MBDEVE', 'DEVE06', 'P7'),
+(51, 'FEDEVE', 'DEVE01', 'P2'),
+(52, 'FEDEVE', 'DEVE02', 'P3'),
+(53, 'FEDEVE', 'DEVE03', 'P4'),
+(54, 'FEDEVE', 'DEVE04', 'P5'),
+(55, 'FEDEVE', 'DEVE05', 'P6'),
+(56, 'FEDEVE', 'DEVE06', 'P7'),
+(57, 'TEST', 'TEST01', 'P2'),
+(58, 'TEST', 'TEST02', 'P3'),
+(59, 'TEST', 'TEST03', 'P4'),
+(60, 'TEST', 'TEST04', 'P5'),
+(61, 'TEST', 'TEST05', 'P6'),
+(62, 'TEST', 'TEST06', 'P7'),
+(63, 'AWSC', 'AWSC01', 'P5'),
+(64, 'AWSC', 'AWSC02', 'P6'),
+(65, 'AWSC', 'AWSC03', 'P7'),
+(66, 'AWSC', 'AWSC04', 'P8'),
+(67, 'DSEN', 'DSEN01', 'P4'),
+(68, 'DSEN', 'DSEN02', 'P5'),
+(69, 'DSEN', 'DSEN03', 'P6'),
+(70, 'DSEN', 'DSEN04', 'P7'),
+(71, 'DSEN', 'DSEN05', 'P8'),
+(72, 'DSAN', 'DSAN01', 'P4'),
+(73, 'DSAN', 'DSAN02', 'P5'),
+(74, 'DSAN', 'DSAN03', 'P6'),
+(75, 'DSAN', 'DSAN04', 'P7'),
+(76, 'DSAN', 'DSAN05', 'P8'),
+(77, 'DEVO', 'DEVO01', 'P3'),
+(78, 'DEVO', 'DEVO02', 'P4'),
+(79, 'DEVO', 'DEVO03', 'P5'),
+(80, 'DEVO', 'DEVO04', 'P6'),
+(81, 'DEVO', 'DEVO05', 'P7'),
+(82, 'DEVO', 'DEVO06', 'P8');
+
+
+INSERT INTO Salary (employeeId, jobRankId) VALUES
+	('anhnh88', '2'),
+	('DE170703', '7'),
+	('HE153678', '15'),
+	('HE161700', '20'),
+	('HE161706', '23'),
+	('HE161755', '28'),
+	('HE163415', '35'),
+	('HE163635', '40'),
+	('HE164044', '39'),
+	('HE170072', '41'),
+	('HE170180', '42'),
+	('HE170201', '43'),
+	('HE170203', '45'),
+	('HE170420', '49'),
+	('HE170595', '51'),
+	('HE171098', '56'),
+	('HE171319', '57'),
+	('HE171320', '61'),
+	('HE171416', '63'),
+	('HE171702', '66'),
+	('HE171937', '67'),
+	('HE172032', '75'),
+	('HE173037', '73'),
+	('HE173046', '78'),
+	('HE176640', '82'),
+	('HE181829', '51'),
+	('HE181879', '56'),
+	('HS170024', '45'),
+	('HS170726', '50'),
+	('HS173175', '43'),
+	('HS176033', '44');
+
+INSERT INTO Salary (employeeId, jobRankId) VALUES
+	('trungnt', '3'),
+	('HE161232', '8'),
+	('HE161643', '14'),
+	('HE163665', '17'),
+	('HE170123', '22'),
+	('HE170268', '29'),
+	('HE170302', '38'),
+	('HE170314', '39'),
+	('HE170317', '40'),
+	('HE170617', '41'),
+	('HE171209', '42'),
+	('HE171310', '43'),
+	('HE171883', '46'),
+	('HE172207', '48'),
+	('HE172366', '52'),
+	('HE172533', '55'),
+	('HE172693', '58'),
+	('HE172802', '60'),
+	('HE173328', '64'),
+	('HE173373', '65'),
+	('HE173476', '70'),
+	('HE173489', '72'),
+	('HE176275', '75'),
+	('HE176282', '78'),
+	('HE176285', '82'),
+	('HE176346', '52'),
+	('HE176360', '54'),
+	('HE176603', '46'),
+	('HE176686', '49'),
+	('HE176834', '41');
+
+INSERT INTO Salary (employeeId, jobRankId) VALUES
+	('thangnv75', '4'),
+	('HE150057', '9'),
+	('HE151095', '13'),
+	('HE153206', '16'),
+	('HE160694', '24'),
+	('HE161357', '33'),
+	('HE161795', '34'),
+	('HE163146', '42'),
+	('HE164035', '39'),
+	('HE170051', '40'),
+	('HE170245', '41'),
+	('HE170422', '43'),
+	('HE170428', '47'),
+	('HE170444', '50'),
+	('HE170533', '53'),
+	('HE170842', '54'),
+	('HE170863', '59'),
+	('HE170907', '62'),
+	('HE170996', '65'),
+	('HE171071', '63'),
+	('HE171073', '71'),
+	('HE171162', '73'),
+	('HE171442', '76'),
+	('HE171482', '79'),
+	('HE171578', '81'),
+	('HE171687', '54'),
+	('HE171851', '52'),
+	('HE171865', '48'),
+	('HE176182', '46'),
+	('HE176697', '42'),
+	('HE176751', '32'),
+	('SE03520', '26'),
+	('SE04495', '21');
+
+INSERT INTO Salary (employeeId, jobRankId) VALUES
+	('sonnt5', '5'),
+	('HE161285', '10'),
+	('HE161816', '12'),
+	('HE163067', '18'),
+	('HE170011', '27'),
+	('HE170167', '30'),
+	('HE170273', '36'),
+	('HE170301', '44'),
+	('HE170415', '39'),
+	('HE170607', '40'),
+	('HE170622', '41'),
+	('HE170631', '42'),
+	('HE170724', '48'),
+	('HE170960', '46'),
+	('HE170999', '54'),
+	('HE171312', '53'),
+	('HE171411', '60'),
+	('HE171540', '57'),
+	('HE171628', '66'),
+	('HE172037', '64'),
+	('HE172748', '69'),
+	('HE172838', '76'),
+	('HE172848', '74'),
+	('HE173248', '79'),
+	('HE173588', '81'),
+	('HE176077', '55'),
+	('HE176087', '51'),
+	('HE176169', '47'),
+	('HE176420', '50'),
+	('HE176882', '44');
+
+INSERT INTO Salary (employeeId, jobRankId) VALUES
+	('tientd17', '6'),
+	('HE150732', '11'),
+	('HE150998', '13'),
+	('HE161224', '19'),
+	('HE161597', '25'),
+	('HE163629', '31'),
+	('HE170089', '37'),
+	('HE170685', '41'),
+	('HE170793', '39'),
+	('HE171358', '40'),
+	('HE171394', '42'),
+	('HE171421', '44'),
+	('HE171552', '49'),
+	('HE171908', '45'),
+	('HE171915', '55'),
+	('HE171916', '52'),
+	('HE171990', '61'),
+	('HE172180', '58'),
+	('HE172280', '64'),
+	('HE172313', '65'),
+	('HE172435', '68'),
+	('HE172527', '74'),
+	('HE172538', '72'),
+	('HE172702', '80'),
+	('HE172737', '80'),
+	('HE173401', '56'),
+	('HE173457', '53'),
+	('HE176211', '49'),
+	('HE176279', '48'),
+	('HE176633', '40'),
+	('HE176747', '43');
